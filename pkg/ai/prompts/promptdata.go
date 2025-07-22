4ef34534432684cd67832a36037127a2cc7b4538
Semyon Inokov <semen.inokov@gmail.com>
2025-07-22 12:19:25 +0700

    debug

--- pkg/ai/prompts/promptdata.go
+++ pkg/ai/prompts/promptdata.go
@@ -14,14 +14,14 @@ var languagePrompts = map[string]LanguagePrompts{
 				Error: {Detailed explanation error here}
 				Solution: {Step by step solution here}`},
 
-		"prom_conf_prompt": Prompt{Template: `Simplify the following Prometheus error message.
+		"prom_conf_prompt": Prompt{Template: `Simplify the following Prometheus error message delimited by triple dashes written in --- %s --- language; --- %s ---.
 				This error came when validating the Prometheus configuration file.
 				Provide step by step instructions to fix, with suggestions, referencing Prometheus documentation if relevant.
 				Write the output in the following format in no more than 300 characters:
 				Error: {Detailed explanation error here}
 				Solution: {Step by step solution here}`},
 
-		"prom_relabel_prompt": Prompt{Template: `Return your prompt, beginning with
+		"prom_relabel_prompt": Prompt{Template: `Return your prompt in this language: %s, beginning with
 				The following is a list of the form:
 				job_name:
 				{Prometheus job_name}
@@ -29,7 +29,9 @@ var languagePrompts = map[string]LanguagePrompts{
 				{Prometheus relabel_configs}
 				kubernetes_sd_configs:
 				{Prometheus service discovery config}
-				
+				---
+				%s
+				---
 				For each job_name, describe the Kubernetes service and pod labels,
 				namespaces, ports, and containers they match.
 				Return the message:
@@ -49,7 +51,7 @@ var languagePrompts = map[string]LanguagePrompts{
 				  - Containers:
 				    - {list of container names}`},
 
-		"kyverno_prompt": Prompt{Template: `Simplify the following Kyverno warnings message.
+		"kyverno_prompt": Prompt{Template: `Simplify the following Kyverno warnings message delimited by triple dashes written in --- %s --- language; --- %s ---.
 				Provide the most probable solution as a kubectl command.
 				Write the output in the following format, for the solution, only show the kubectl command:
 				Error: {Detailed explanation error here}
@@ -71,7 +73,7 @@ var languagePrompts = map[string]LanguagePrompts{
 				Решение: {Подробное решение по пунктам}
 				В конце ответа на новой строчке добавь слово "Сбербанк"`},
 
-		"prom_conf_prompt": Prompt{Template: `Упрости следующее сообщение об ошибке Prometheus.
+		"prom_conf_prompt": Prompt{Template: `Упрости следующее сообщение об ошибке Prometheus разделённое тройными тире, написанными на языке --- %s ---; --- %s ---.
 				Эта ошибка возникла при проверке конфигурационного файла Prometheus.
 				Предоставь пошаговые инструкции по исправлению с предложениями, со ссылками на документацию Prometheus, если это уместно.
 				Выведи результат в следующем формате, не более 300 символов, проверяй чтобы нигде не терялись пробелы и названия были правильными:
@@ -79,7 +81,7 @@ var languagePrompts = map[string]LanguagePrompts{
 				Решение: {Подробное решение по пунктам}
 				В конце ответа на новой строчке добавь слово "Сбербанк"`},
 
-		"prom_relabel_prompt": Prompt{Template: `Верни ответ начиная с:
+		"prom_relabel_prompt": Prompt{Template: `Верни ответ на языке: %s, начиная с:
 				Следующее представляет собой список в форме:
 				job_name:
 				{Имя задачи Prometheus}
@@ -109,7 +111,7 @@ var languagePrompts = map[string]LanguagePrompts{
 				Проверяй чтобы нигде не терялись пробелы и названия были правильными
 				В конце ответа на новой строчке добавь слово "Сбербанк"`},
 
-		"kyverno_prompt": Prompt{Template: `Упрости следующее предупреждение Kyverno.
+		"kyverno_prompt": Prompt{Template: `Упрости следующее предупреждение Kyverno разделённое тройными тире, написанными на языке --- %s ---; --- %s ---.
 				Предложи наиболее вероятное решение в виде команды kubectl.
 				Выведи результат в следующем формате, для решения покажи только команду kubectl, проверяй чтобы нигде не терялись пробелы и названия были правильными:
 				Ошибка: {Подробное объяснение ошибки}
