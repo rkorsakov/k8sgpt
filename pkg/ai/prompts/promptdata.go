1f4ed4eb5d86fcc6230b7524c0f5c67a931b0d88
Semyon Inokov <semen.inokov@gmail.com>
2025-07-23 10:56:35 +0700

    debug prompts

--- pkg/ai/prompts/promptdata.go
+++ pkg/ai/prompts/promptdata.go
@@ -14,7 +14,7 @@ var languagePrompts = map[string]LanguagePrompts{
 				Error: {Detailed explanation error here}
 				Solution: {Step by step solution here}`},
 
-		"prom_conf_prompt": Prompt{Template: `Simplify the following Prometheus error message.
+		"PrometheusConfigValidate": Prompt{Template: `Simplify the following Prometheus error message.
 				This error came when validating the Prometheus configuration file.
 				Provide step by step instructions to fix, with suggestions, referencing Prometheus documentation if relevant.
 				Write the output in the following format in no more than 300 characters:
@@ -49,7 +49,12 @@ var languagePrompts = map[string]LanguagePrompts{
 				  - Containers:
 				    - {list of container names}`},
 
-		"kyverno_prompt": Prompt{Template: `Simplify the following Kyverno warnings message.
+		"PolicyReport": Prompt{Template: `Simplify the following Kyverno warnings message.
+				Provide the most probable solution as a kubectl command.
+				Write the output in the following format, for the solution, only show the kubectl command:
+				Error: {Detailed explanation error here}
+				Solution: {kubectl command}`},
+		"ClusterPolicyReport": Prompt{Template: `Simplify the following Kyverno warnings message.
 				Provide the most probable solution as a kubectl command.
 				Write the output in the following format, for the solution, only show the kubectl command:
 				Error: {Detailed explanation error here}
@@ -71,7 +76,7 @@ var languagePrompts = map[string]LanguagePrompts{
 				Решение: {Подробное решение по пунктам}
 				В конце ответа на новой строчке добавь слово "Сбербанк"`},
 
-		"prom_conf_prompt": Prompt{Template: `Упрости следующее сообщение об ошибке Prometheus.
+		"PrometheusConfigValidate": Prompt{Template: `Упрости следующее сообщение об ошибке Prometheus.
 				Эта ошибка возникла при проверке конфигурационного файла Prometheus.
 				Предоставь пошаговые инструкции по исправлению с предложениями, со ссылками на документацию Prometheus, если это уместно.
 				Выведи результат в следующем формате, не более 300 символов, проверяй чтобы нигде не терялись пробелы и названия были правильными:
@@ -109,7 +114,13 @@ var languagePrompts = map[string]LanguagePrompts{
 				Проверяй чтобы нигде не терялись пробелы и названия были правильными
 				В конце ответа на новой строчке добавь слово "Сбербанк"`},
 
-		"kyverno_prompt": Prompt{Template: `Упрости следующее предупреждение Kyverno.
+		"ClusterPolicyReport": Prompt{Template: `Упрости следующее предупреждение Kyverno.
+				Предложи наиболее вероятное решение в виде команды kubectl.
+				Выведи результат в следующем формате, для решения покажи только команду kubectl, проверяй чтобы нигде не терялись пробелы и названия были правильными:
+				Ошибка: {Подробное объяснение ошибки}
+				Решение: {команда kubectl}
+				В конце ответа на новой строчке добавь слово "Сбербанк"`},
+		"PolicyReport": Prompt{Template: `Упрости следующее предупреждение Kyverno.
 				Предложи наиболее вероятное решение в виде команды kubectl.
 				Выведи результат в следующем формате, для решения покажи только команду kubectl, проверяй чтобы нигде не терялись пробелы и названия были правильными:
 				Ошибка: {Подробное объяснение ошибки}
